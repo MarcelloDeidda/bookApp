6e9ab28f6ebdf8f515fe4cb4d97fe7556f4e7545
MarcelloDeidda <marcello35@hotmail.it>
2022-11-01 09:08:52 +0000

    database design

---
+++ models/book.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Review = require("./review");
+
+const bookSchema = new Schema({
+    title: String,
+    author: String,
+    year: Number,
+    imgUrl: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review"
+        }
+    ]
+});
+
+bookSchema.post("findOneAndDelete", async (doc) => {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
